3d28ea19ba126f81ab9ef2eca86d74edd9afee7a
bee <3172736837@qq.com>
2025-04-11 21:45:27 +0800

    Filter @Query @Transient

---
+++ Movies/Screens/FilterSelectionScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FilterSelectionScreen.swift
+//  Movies
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/4/11.
+//
+
+import SwiftUI
+
+struct FilterSelectionScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterSelectionScreen()
+}
